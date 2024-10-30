12616e05272d362e7d54f7715b409a16612ee467
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-30 15:06:04 +0900

    refactor: 필요없는 try-catch 제거

--- src/main/java/dbdr/security/config/JwtFilter.java
+++ src/main/java/dbdr/security/config/JwtFilter.java
@@ -1,24 +1,18 @@
 package dbdr.security.config;
 
 import dbdr.security.service.JwtProvider;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
-@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
@@ -29,20 +23,10 @@ public class JwtFilter extends OncePerRequestFilter {
 
         String token = jwtProvider.extractToken(request);
 
-        if (token != null) {
-            try {
-                //jwtProvider.validateToken(token);
-                Authentication authentication = jwtProvider.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException |
-                     SignatureException | SecurityException | IllegalArgumentException ex) {
-                throw ex;
-            } catch (Exception e) {
-                log.debug("토큰 유저 정보 추출 실패 : {}", e.getMessage());
-                throw new JwtException("유효하지 않은 토큰입니다.");
-            }
+        if (StringUtils.hasText(token)) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-
         filterChain.doFilter(request, response);
     }
 }
