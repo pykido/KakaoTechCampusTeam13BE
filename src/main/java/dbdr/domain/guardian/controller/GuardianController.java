1643bcaaef5a49c0f809cd842c54dea765f05c02
20jcode <109460399+20jcode@users.noreply.github.com>
2024-10-30 19:04:38 +0900

    [긴급] : 로그인된 멤버 엔티티 받는 커스텀어노테이션 추가 (#67)
    * feat : custom auth member 어노테이션 만들기
    
    * feat : custom auth member 어노테이션 추가
    
    GuardianController에서 테스트 해보았습니다.
    참고하시면 될 듯 합니당 !!!

--- src/main/java/dbdr/domain/guardian/controller/GuardianController.java
+++ src/main/java/dbdr/domain/guardian/controller/GuardianController.java
@@ -2,11 +2,14 @@ package dbdr.domain.guardian.controller;
 
 import dbdr.domain.guardian.dto.request.GuardianRequest;
 import dbdr.domain.guardian.dto.response.GuardianResponse;
+import dbdr.domain.guardian.entity.Guardian;
 import dbdr.domain.guardian.service.GuardianService;
+import dbdr.security.LoginGuardian;
+import dbdr.security.dto.BaseUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,13 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/${spring.app.version}/guardian")
 @RequiredArgsConstructor
+@Slf4j
 public class GuardianController {
 
     private final GuardianService guardianService;
 
     @GetMapping("/{guardianId}")
     public ResponseEntity<GuardianResponse> showGuardianInfo(
-        @PathVariable("guardianId") Long guardianId) {
+        @PathVariable("guardianId") Long guardianId, @LoginGuardian Guardian gaurdian) {
+        log.info("guardianId: {}", gaurdian.getName());
         GuardianResponse guardianResponse = guardianService.getGuardianById(guardianId);
         return ResponseEntity.ok(guardianResponse);
     }
